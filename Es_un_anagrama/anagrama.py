ba7c9c18480fdb13f11b3ff007e6b18285b13d7e
Arnau Nin <ninbsnss92@gmail.com>
2024-10-05 20:20:48 +0200

    Full code

--- Es_un_anagrama/anagrama.py
+++ Es_un_anagrama/anagrama.py
@@ -1,1 +1,14 @@
+def es_un_anagrama(word1, word2):
+    word1 = word1.lower()
+    word2 = word2.lower()
 
+    if len(word1) != len(word2) or word1 == word2:
+        return False
+    else:
+        for letter in word1:
+            if letter not in word2:
+                return False
+            
+        return True
+    
+print(es_un_anagrama('castor','Castro'))
