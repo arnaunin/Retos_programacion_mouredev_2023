abedcb9576ae0bde26378e1339fcb99a0d0cfb16
Arnau Nin <ninbsnss92@gmail.com>
2024-10-08 18:14:21 +0200

    Codigo numeros primos

--- retos/Numeros_primos/numeros_primos.py
+++ retos/Numeros_primos/numeros_primos.py
@@ -1,1 +1,11 @@
+def es_primo(n):
+    for i in range(2, n):
+        if n % i == 0: # Si la división de ese numero por algun otro numero da 0, el numero no puede ser primo y se retorna False
+            return False
+    
+    return True # Si ha completado el bucle significa que no tiene ningún dividendo a parte de 1 y el mismo por lo tanto es primo
+
+for i in range(2, 101):
+    if es_primo(i):
+        print(i)
 
