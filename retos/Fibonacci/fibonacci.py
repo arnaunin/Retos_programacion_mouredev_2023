199ca04fa0435d2f8c42da81249b885cd7f57068
Arnau Nin <ninbsnss92@gmail.com>
2024-10-08 17:48:28 +0200

    Fibonacci code

--- retos/Fibonacci/fibonacci.py
+++ retos/Fibonacci/fibonacci.py
@@ -1,1 +1,12 @@
+def fibonacci(n):
 
+    # Inicializamos los dos primeros valores de la sucessión
+    a = 0 # Pirmer valor
+    b = 1 # segundo valor
+
+    # Vamos a recorrer el bucle n veces para imprimir los n primeros valores
+    for i in range(n):
+        print(f"{i+1}: {a}")
+        a, b = b, a + b # El primer valor pasa a ser el segundo valor y el segundo pasa a ser la suma del primero y el segundo
+
+fibonacci(50)
