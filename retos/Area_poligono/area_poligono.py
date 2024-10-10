97cff2bd4e438733c292976563946923049aa179
Arnau Nin <ninbsnss92@gmail.com>
2024-10-10 17:06:38 +0200

    Codigo de area poligono

--- retos/Area_poligono/area_poligono.py
+++ retos/Area_poligono/area_poligono.py
@@ -1,1 +1,22 @@
+def area_poligono(poligono):
+    try:
+        if poligono == "cuadrado":
+            lado = int(input("Introduce el tamaño de los lados: "))
+            area = lado*lado
+            return area
+        elif poligono == "rectangulo":
+            ancho = int(input("Introduce el tamaño del ancho: "))
+            largo = int(input("Introduce el tamaño del largo: "))
+            area = ancho*largo
+            return area
+        elif poligono == "triangulo":
+            base = int(input("Introduce el tamaño de la base: "))
+            altura = int(input("Introduce el tamaño de la altura: "))
+            area = (base*altura)/2
+            return area
+    except:
+        print(ValueError)
 
+print(area_poligono("triangulo"))
+print(area_poligono("cuadrado"))
+print(area_poligono("rectangulo"))
