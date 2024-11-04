68156557a86b6f8aad9a025c1aa5d9e854982b9b
Arnau Nin <ninbsnss92@gmail.com>
2024-11-04 09:18:23 +0100

    Finish creando caracteres

--- retos/Eliminando_caracteres/eliminando_caracteres.py
+++ retos/Eliminando_caracteres/eliminando_caracteres.py
@@ -1,6 +1,4 @@
 def eliminar_caracteres(str1, str2):
-    out1 = ''
-    out2 = ''
 
     out1 = ''.join(caracter for caracter in str1 if caracter not in str2)
     out2 = ''.join(caracter for caracter in str2 if caracter not in str1)
@@ -8,4 +6,4 @@ def eliminar_caracteres(str1, str2):
     print('Out1:', out1)
     print('Out2:', out2)
 
-eliminar_caracteres('hola','adeu')
+eliminar_caracteres('python','caracteres')
